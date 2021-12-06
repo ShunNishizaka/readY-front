fb52e664fdb6edaa1c6621cc5fbae326dda8fe5c
ShunNishizaka <te180581@gmail.com>
2021-12-06 10:44:49 +0900

    フッターを追記

--- src/pages/bookDetail.jsx
+++ src/pages/bookDetail.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import Header from '../modules/header.jsx'
 import Detail from '../modules/detail'
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
+import Footer from "../modules/footer.jsx";
 
 function BookDetail() {
 	return (
@@ -10,6 +11,7 @@ function BookDetail() {
 			<Button color="inherit" size="large" to="/mypage" component={Link}>マイページへ</Button>
 			</Header>
 			<Detail />
+			<Footer />
 		</div>
 	)
 }
