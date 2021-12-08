e0b1ee7e6e200f3ccd25f36d2568d607647e5e61
ShunNishizaka <te180581@gmail.com>
2021-12-08 11:19:58 +0900

    名前間違えてました

--- src/modules/experimentBookCard.jsx
+++ src/modules/experimentBookCard.jsx
@@ -5,12 +5,12 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { useState } from 'react';
 import Card from '@mui/material/Card';
 import BookInfoIcons from './bookInfoIcons';
 
 
-export default function ExperimentBookInfo() {
+
+export default function ExperimentBookCard() {
 
     return(
       <Grid item xs={12} sm={6} md={2.8} >
@@ -29,7 +29,7 @@ export default function ExperimentBookInfo() {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button variant="contained">この書籍のグループを検索</Button>
+            <Button variant="contained" >この書籍のグループを検索</Button>
         </CardActions>
       </Card>
      </Grid>
