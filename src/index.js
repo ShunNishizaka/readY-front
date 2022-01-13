0875e8d704f9c051ad8d3c3b694ad608c4f55f9c
Lazialize <lazinelia@gmail.com>
2022-01-13 14:29:30 +0900

    Delete All

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-import LandingPage from './pages/landingPage.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from './pages/loginPage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MyPage from './pages/myPage.jsx';
-import BookDetail from './pages/bookDetail.jsx';
-import SearchResult from './pages/searchResult.jsx';
 
 ReactDOM.render(
 	<Router>
 		<Routes>
-			<Route path="/" element={<LandingPage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/mypage" element={<MyPage />} />
-			<Route path="/bookdetail" element={<BookDetail />} />
-			<Route path="/searchresult" element={<SearchResult />} />
 		</Routes>
 	</Router>,
 	document.getElementById('root')
