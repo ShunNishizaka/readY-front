a8baede1ba43365f2b391360797c71b77b8883c6
Lazialize <lazinelia@gmail.com>
2022-01-19 06:50:16 +0000

    ページ遷移用のボタンのハリボテ

--- src/pages/search-result/index.jsx
+++ src/pages/search-result/index.jsx
@@ -1,4 +1,6 @@
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 import Paper from '@mui/material/Paper'
 import React, { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
@@ -7,6 +9,14 @@ import CardList from '../../components/blocks/CardList'
 import FlexBox from '../../components/atoms/FlexBox'
 import Header from '../../components/blocks/Header'
 import * as Request from '../../utils/request'
+
+const range = (start, end) => {
+  const list = []
+  for (let i = start; i <= end; i++) {
+    list.push(i)
+  }
+  return list
+}
 
 export default function SearchResult () {
   const navigate = useNavigate()
@@ -44,6 +54,19 @@ export default function SearchResult () {
       <Header searchBox placeholder="書籍を検索..." keyPress={keyPressed}/>
       <Box sx={{ flex: 1, height: 'auto', p: 3 }} component={Paper} square>
         {searchPending ? 'けんさくちう...' : <CardList bookInfos={searchResults.books} text="検索ワードに該当する書籍が見つかりませんでした。"/>}
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', p: 3, flexDirection: 'column' }} component={Paper} square>
+        <ButtonGroup>
+          {range(1, searchResults.page_count).map((value, index) => {
+            return (
+              <Button
+                key={index}
+                variant={value === searchResults.page ? 'outlined' : 'contained'}>
+                  {value}
+              </Button>
+            )
+          })}
+        </ButtonGroup>
       </Box>
     </FlexBox>
   )
